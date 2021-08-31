585faf443eb105318cca94c96028bd36264a4d48
ABDUL AHAD RAZA <89089753+PROGRAMMINGSITE@users.noreply.github.com>
2021-08-31 04:51:42 -0700

    Delete selenium loop.py

--- selenium loop.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome('C:/Users/AHAD RAZA/Desktop/chromedriver.exe')
-for i in range(5):
-    loop=driver.get('https://www.youtube.com/')
-    driver.implicitly_wait(4)
